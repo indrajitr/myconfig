ee3127afb8805903561db9cd14390d848bcbf65c
Indrajit Raychaudhuri <irc@indrajit.com>
2011-04-09 15:03:17 +0530

    Cleanups

--- userhome/.oh-my-zsh/custom/misc.zsh
+++ userhome/.oh-my-zsh/custom/misc.zsh
@@ -2,7 +2,6 @@
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 plugins=($plugins osx ssh-agent brew git github svn textmate gem ruby rails3)
-typeset -U plugins
 
 # Set theme
 # export ZSH_THEME="jreese"
@@ -11,8 +10,6 @@ export ZSH_THEME="gallifrey"
 
 # Customize CDPATH
 cdpath=($cdpath .. ~ ~/Projects/Libraries ~/Projects/Business ~/Projects/Community ~/Projects/Personal)
-typeset -U cdpath
-#export CDPATH=.:..:~:~/Projects/Libraries:~/Projects/Business:~/Projects/Community:~/Projects/Personal:$CDPATH
 
 
 # unsetopt complete_aliases
